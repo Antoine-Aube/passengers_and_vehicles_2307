615d2b29d9d2bc17425cdb0fc1370d6386393008
Antoine Aube <aaube3@gmail.com>
2023-07-19 09:16:26 -0600

    feat: #adult?

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -3,7 +3,15 @@ class Passenger
               :age
 
   def initialize(passenger_info)
-    @name = passenger_info[:name]
-    @age = passenger_info[:age]
+    @name = passenger_info["name"]
+    @age = passenger_info["age"]
+  end
+
+  def adult?
+    if @age >= 18
+      true 
+    else 
+      false
+    end
   end
 end
