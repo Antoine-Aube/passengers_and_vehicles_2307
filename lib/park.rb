39fe5cdb30031a0845215c78ff91600859517e6a
Antoine Aube <aaube3@gmail.com>
2023-07-19 10:25:41 -0600

    feat: #calculate_revenue

--- lib/park.rb
+++ lib/park.rb
@@ -2,6 +2,7 @@ class Park
   attr_reader :name,
               :admission_price,
               :vehicles
+          
   
   def initialize(name, admission_price)
     @name = name 
@@ -11,5 +12,23 @@ class Park
 
   def administer_vehicle(vehicle)
     @vehicles << vehicle
+  end
+
+  def list_visitors
+    visitors = []
+    @vehicles.each do |vehicle|
+      vehicle.passengers.each do |passenger|
+        visitors << passenger
+      end
+    end
+    visitors.flatten!
+    visitors
+  end
+
+  def calculate_revenue
+    visitors = list_visitors 
+    adult_visitors = visitors.find_all { |visitor| visitor.adult? }
+    revenue = adult_visitors.length * @admission_price
+    revenue
   end
 end
